e94d1f95ccd9efa5bcdc9aa7e10396793722da79
gisellabranco <159316919+gisellabranco@users.noreply.github.com>
2024-03-26 18:00:16 -0300

    Add files via upload

---
+++ python execução.py
@@ -0,0 +1,19 @@
+print ("Gisella")
+
+numero = 13
+nome = "Gisella linda"
+booleano = True
+
+numero_real = 13.0
+
+resposta = ( 2 < 4 )or ( 4 == 5)
+
+# ordem de execução em Python segue este fluxo geral:
+#1.Processamento de linhas de comando e importações.
+#2.Definição de funções e classes.
+#3.Execução do código principal no escopo global.
+#4.Chamadas de funções, com a execução do código dentro delas.
+#5.Execução de chamadas de função recursivas, se houver.
+#6.Aplicação de instruções de controle de fluxo, como if, elif, else, for, while.
+#7.Tratamento de exceções, se ocorrerem.
+#8.Finalização da execução, a menos que haja mais código a ser executado além do final do programa.

ad553bd9b7d478516b13c0d8a2f4b64f8d67adf0
gisellabranco <159316919+gisellabranco@users.noreply.github.com>
2024-03-26 23:05:33 -0300

    Add files via upload

--- python execução.py
+++ python execução.py
@@ -17,3 +17,25 @@ resposta = ( 2 < 4 )or ( 4 == 5)
 #6.Aplicação de instruções de controle de fluxo, como if, elif, else, for, while.
 #7.Tratamento de exceções, se ocorrerem.
 #8.Finalização da execução, a menos que haja mais código a ser executado além do final do programa.
+
+
+
+
+
+## Ordem de Precedência dos Operadores Lógicos:
+
+#A ordem de precedência dos operadores lógicos em Python é a seguinte:
+
+#1. Parênteses ( )
+#2. Not (not)
+#3. And (and)
+#4. Or (or)
+
+## Exemplo de Modificação da Ordem das Operações Lógicas:
+
+
+
+# Aqui a expressão será avaliada como True, porque 'False and True' é False, e 'True or False' é True.
+resposta_modificada = (2 < 4) and (4 == 5) or (6 > 3)
+
+print(resposta_modificada)  # Output: True
